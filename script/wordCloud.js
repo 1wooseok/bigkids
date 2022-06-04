a6cc8f7c24c8a527b2400f3f109d9cd6be651408
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-06-05 01:38:04 +0900

    save

--- script/wordCloud.js
+++ script/wordCloud.js
@@ -2,13 +2,17 @@ let margin = { top: 10, right: 10, bottom: 10, left: 10 };
 const WINDOW_WIDTH = window.innerWidth;
 const WINDOW_HEIGHT = window.innerHeight;
 let width =
-  WINDOW_WIDTH < 1500
+  WINDOW_WIDTH < 850
+    ? WINDOW_WIDTH
+    : WINDOW_WIDTH < 1500
     ? WINDOW_WIDTH / 2
     : WINDOW_WIDTH < 1800
     ? WINDOW_WIDTH / 2.5
     : WINDOW_WIDTH / 3;
 let height =
-  WINDOW_WIDTH < 1500
+  WINDOW_WIDTH < 850
+    ? WINDOW_WIDTH
+    : WINDOW_WIDTH < 1500
     ? WINDOW_WIDTH / 2
     : WINDOW_WIDTH < 1800
     ? WINDOW_WIDTH / 2.5
@@ -20,7 +24,7 @@ height = height - margin.top - margin.bottom;
 let svg = d3
   .select("#word-cloud")
   .append("svg")
-  .attr("viewBox", `0 0 ${width * 1.12} ${height * 1.12}`);
+  .attr("viewBox", `0 0 ${width} ${height}`);
 
 let layout = d3.layout
   .cloud()
@@ -48,7 +52,7 @@ function draw(words) {
     .attr("text-anchor", "middle")
     .attr(
       "transform",
-      (d) => `translate(${[d.x, d.y / 1.12]}) rotate(${d.rotate})`
+      (d) => `translate(${[d.x, d.y / 1.12]})`
     ) // rotate(${d.rotate})
     .text(function (d) {
       return d.text;
